e697bcf8c24e4710396a6316b961eb49e26843d8
GuillaumeC96 <122291329+GuillaumeC96@users.noreply.github.com>
2023-02-28 10:49:48 +0100

    Update bonjour.php

--- bonjour.php
+++ bonjour.php
@@ -9,72 +9,18 @@
 <body>
 <?php
 
+$departements = array(
+    "Hauts-de-france" => array("Aisne", "Nord", "Oise", "Pas-de-Calais", "Somme"),
+    "Bretagne" => array("Côtes d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan"),
+    "Grand-Est" => array("Ardennes", "Aube", "Marne", "Haute-Marne", "Meurthe-et-Moselle", "Meuse", "Moselle", "Bas-Rhin", "Haut-Rhin", "Vosges"),
+    "Normandie" => array("Calvados", "Eure", "Manche", "Orne", "Seine-Maritime")
+);
 
-
-$pays = array (
-    "Bucarest" => "Roumanie",
-    "Bruxelles" => "Belgique",
-    "Oslo" => "Norvège",
-    "Ottawa" => "Canada",
-    "Paris" => "France",
-    "Port-au-Prince" => "Haïti",
-    "Port-d'Espagne" => "Trinité-et-Tobago",
-    "Prague" => "République tchèque",
-    "Rabat" => "Maroc",
-    "Riga" => "Lettonie",
-    "Rome" => "Italie",
-    "San José" => "Costa Rica",
-    "Santiago" => "Chili",
-    "Sofia" => "Bulgarie",
-    "Alger" => "Algérie",
-    "Amsterdam" => "Pays-Bas",
-    "Andorre-la-Vieille" => "Andorre",
-    "Asuncion" => "Paraguay",
-    "Athènes" => "Grèce",
-    "Bagdad" => "Irak",
-    "Bamako" => "Mali",
-    "Berlin" => "Allemagne",
-    "Bogota" => "Colombie",
-    "Brasilia" => "Brésil",
-    "Bratislava" => "Slovaquie",
-    "Varsovie" => "Pologne",
-    "Budapest" => "Hongrie",
-    "Le Caire" => "Egypte",
-    "Canberra" => "Australie",
-    "Caracas" => "Venezuela",
-    "Jakarta" => "Indonésie",
-    "Kiev" => "Ukraine",
-    "Kigali" => "Rwanda",
-    "Kingston" => "Jamaïque",
-    "Lima" => "Pérou",
-    "Londres" => "Royaume-Uni",
-    "Madrid" => "Espagne",
-    "Malé" => "Maldives",
-    "Mexico" => "Mexique",
-    "Minsk" => "Biélorussie",
-    "Moscou" => "Russie",
-    "Nairobi" => "Kenya",
-    "New Delhi" => "Inde",
-    "Stockholm" => "Suède",
-    "Téhéran" => "Iran",
-    "Tokyo" => "Japon",
-    "Tunis" => "Tunisie",
-    "Copenhague" => "Danemark",
-    "Dakar" => "Sénégal",
-    "Damas" => "Syrie",
-    "Dublin" => "Irlande",
-    "Erevan" => "Arménie",
-    "La Havane" => "Cuba",
-    "Helsinki" => "Finlande",
-    "Islamabad" => "Pakistan",
-    "Vienne" => "Autriche",
-    "Vilnius" => "Lituanie",
-    "Zagreb" => "Croatie"); 
-
+ksort($departements);
 
      // Lecture de chaque lignes du tableau
-    foreach($pays as $cle=>$valeur){
-        if(substr($cle,0,1)=="B"){unset($cle);} else {echo $cle.' : '.$valeur.'<br>';}
+    foreach($departements as $cle=>$valeur){
+       echo $cle." : ".implode(", ",$valeur)."<br>";
      }
 
 
